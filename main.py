21c05edd09cb97f81d45e88e0c40bc54ad202fbd
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-11-27 16:13:52 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
+def on_button_pressed_a():
+    music.set_volume(255)
+    music.change_tempo_by(140)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def noteToFrequency(text: str):
-    return 0
+    if text == "A4":
+        return 440
+    return invalid_note
+invalid_note = 0
+notes = [131,
+    139,
+    147,
+    156,
+    165,
+    175,
+    185,
+    196,
+    208,
+    220,
+    233,
+    247,
+    262,
+    277,
+    294,
+    311,
+    330,
+    349,
+    370,
+    392,
+    415,
+    440,
+    466,
+    494,
+    523,
+    554,
+    587,
+    622,
+    659,
+    698,
+    740,
+    784,
+    831,
+    880,
+    932,
+    988]
+invalid_note = -1
+current_note = 0
--- main.ts
+++ main.ts
@@ -1,10 +1,50 @@
-function noteToFrequency (text: string) {
-    if (text == "A4") {
-        return 440
-    }
-    return invalid_note
+input.onPinPressed(TouchPin.P0, function () {
+    play_song()
+})
+function display_current_note () {
+    music.playTone(notes[current_note], music.beat(BeatFraction.Half))
 }
-let invalid_note = 0
-invalid_note = -1
-let notes = [131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 0, 0, 0, 0, 0]
-music.playTone(noteToFrequency("A4"), music.beat(BeatFraction.Whole))
+input.onButtonPressed(Button.A, function () {
+    lower_note()
+})
+function lower_note () {
+    if (current_note > 0) {
+        current_note = current_note - 1
+    }
+    display_current_note()
+}
+input.onPinPressed(TouchPin.P2, function () {
+    song.pop()
+})
+function save_current_note () {
+    song.push(notes[current_note])
+}
+input.onButtonPressed(Button.AB, function () {
+    save_current_note()
+})
+input.onButtonPressed(Button.B, function () {
+    higher_note()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showNumber(song.length)
+})
+function play_song () {
+    for (let value of song) {
+        music.playTone(value, music.beat(BeatFraction.Whole))
+    }
+}
+function higher_note () {
+    if (current_note < notes.length) {
+        current_note = current_note + 1
+    }
+    display_current_note()
+}
+let song: number[] = []
+let current_note = 0
+let notes: number[] = []
+music.setVolume(255)
+music.changeTempoBy(140)
+notes = [131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988]
+let invalid_note = -1
+current_note = 0
+song = []
