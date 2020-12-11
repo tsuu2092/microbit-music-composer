9212061e5b7340a81d8a9af349b6c80f4c0189bd
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-12-11 16:06:14 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,50 +1,101 @@
-input.onPinPressed(TouchPin.P0, function () {
-    play_song()
-})
-input.onButtonPressed(Button.A, function () {
-    lower_note()
-})
-function lower_note () {
-    if (current_note > 0) {
-        current_note = current_note - 1
-    }
-    play_current_note()
-}
-input.onPinPressed(TouchPin.P2, function () {
+def delete_last_note():
     song.pop()
-})
-function save_current_note () {
-    song.push(notes[current_note])
-}
-input.onButtonPressed(Button.AB, function () {
-    save_current_note()
-})
-input.onButtonPressed(Button.B, function () {
-    higher_note()
-})
-input.onPinPressed(TouchPin.P1, function () {
-    basic.showNumber(current_note)
-})
-function play_current_note () {
-    music.playTone(notes[current_note], music.beat(BeatFraction.Half))
-}
-function play_song () {
-    for (let value of song) {
-        music.playTone(value, music.beat(BeatFraction.Half))
-    }
-}
-function higher_note () {
-    if (current_note < notes.length - 1) {
-        current_note = current_note + 1
-    }
+    basic.show_icon(IconNames.NO)
+    basic.pause(100)
+    basic.show_icon(IconNames.EIGTH_NOTE)
+
+def on_button_pressed_a():
+    lower_note()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def lower_note():
+    global current_note
+    if current_note > 0:
+        current_note = current_note - 1
     play_current_note()
-}
-let song: number = []
-let current_note: number = []
-let notes: number = []
-music.setVolume(255)
-music.changeTempoBy(140)
-notes = [131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1046]
-let invalid_note = -1
+
+def on_pin_pressed_p2():
+    delete_last_note()
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def save_current_note():
+    song.append(notes[current_note])
+    play_current_note()
+
+def on_button_pressed_ab():
+    save_current_note()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    higher_note()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_pin_pressed_p1():
+    add_rest()
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+def play_current_note():
+    music.play_tone(notes[current_note], music.beat(BeatFraction.QUARTER))
+
+def on_gesture_shake():
+    play_song()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def add_rest():
+    song.append(0)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    basic.pause(100)
+    basic.show_icon(IconNames.EIGTH_NOTE)
+def play_song():
+    for value in song:
+        music.play_tone(value, music.beat(BeatFraction.HALF))
+def higher_note():
+    global current_note
+    if current_note < len(notes) - 1:
+        current_note = current_note + 1
+    play_current_note()
+song: List[number] = []
+current_note = 0
+notes: List[number] = []
+basic.show_icon(IconNames.EIGTH_NOTE)
+music.set_volume(255)
+music.set_tempo(120)
+notes = [131,
+    139,
+    147,
+    156,
+    165,
+    175,
+    185,
+    196,
+    208,
+    220,
+    233,
+    247,
+    262,
+    277,
+    294,
+    311,
+    330,
+    349,
+    370,
+    392,
+    415,
+    440,
+    466,
+    494,
+    523,
+    554,
+    587,
+    622,
+    659,
+    698,
+    740,
+    784,
+    831,
+    880,
+    932,
+    988,
+    1046]
 current_note = 12
 song = []
--- main.ts
+++ main.ts
@@ -1,54 +1,71 @@
-function delete_last_note () {
-    song.pop()
+function delete_last_note() {
+    _py.py_array_pop(song)
+    basic.showIcon(IconNames.No)
+    basic.pause(100)
+    basic.showIcon(IconNames.EigthNote)
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     lower_note()
 })
-function lower_note () {
+function lower_note() {
+    
     if (current_note > 0) {
         current_note = current_note - 1
     }
+    
     play_current_note()
 }
-input.onPinPressed(TouchPin.P2, function () {
+
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
     delete_last_note()
 })
-function save_current_note () {
+function save_current_note() {
     song.push(notes[current_note])
     play_current_note()
 }
-input.onButtonPressed(Button.AB, function () {
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     save_current_note()
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     higher_note()
 })
-input.onPinPressed(TouchPin.P1, function () {
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
     add_rest()
 })
-function play_current_note () {
+function play_current_note() {
     music.playTone(notes[current_note], music.beat(BeatFraction.Quarter))
 }
-input.onGesture(Gesture.Shake, function () {
+
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
     play_song()
 })
-function add_rest () {
+function add_rest() {
     song.push(0)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(100)
+    basic.showIcon(IconNames.EigthNote)
 }
-function play_song () {
+
+function play_song() {
     for (let value of song) {
         music.playTone(value, music.beat(BeatFraction.Half))
     }
 }
-function higher_note () {
+
+function higher_note() {
+    
     if (current_note < notes.length - 1) {
         current_note = current_note + 1
     }
+    
     play_current_note()
 }
-let song: number[] = []
+
+let song : number[] = []
 let current_note = 0
-let notes: number[] = []
+let notes : number[] = []
 basic.showIcon(IconNames.EigthNote)
 music.setVolume(255)
 music.setTempo(120)
