040222355ae4bfc378835accc6ecccb5b0021a5b
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-12-11 16:09:22 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,59 @@
-function delete_last_note() {
-    _py.py_array_pop(song)
-    basic.showIcon(IconNames.No)
-    basic.pause(100)
-    basic.showIcon(IconNames.EigthNote)
+function delete_last_note () {
+    if (song.length > 0) {
+        music.playTone(song.pop(), music.beat(BeatFraction.Quarter))
+    }
 }
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     lower_note()
 })
-function lower_note() {
-    
+function lower_note () {
     if (current_note > 0) {
         current_note = current_note - 1
     }
-    
     play_current_note()
 }
-
-input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+input.onPinPressed(TouchPin.P2, function () {
     delete_last_note()
 })
-function save_current_note() {
+function save_current_note () {
     song.push(notes[current_note])
     play_current_note()
 }
-
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     save_current_note()
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     higher_note()
 })
-input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+input.onPinPressed(TouchPin.P1, function () {
     add_rest()
 })
-function play_current_note() {
+function play_current_note () {
     music.playTone(notes[current_note], music.beat(BeatFraction.Quarter))
 }
-
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function () {
     play_song()
 })
-function add_rest() {
+function add_rest () {
     song.push(0)
     basic.showIcon(IconNames.SmallDiamond)
     basic.pause(100)
     basic.showIcon(IconNames.EigthNote)
 }
-
-function play_song() {
+function play_song () {
     for (let value of song) {
         music.playTone(value, music.beat(BeatFraction.Half))
     }
 }
-
-function higher_note() {
-    
+function higher_note () {
     if (current_note < notes.length - 1) {
         current_note = current_note + 1
     }
-    
     play_current_note()
 }
-
-let song : number[] = []
 let current_note = 0
-let notes : number[] = []
+let notes: number[] = []
+let song : number[] = []
 basic.showIcon(IconNames.EigthNote)
 music.setVolume(255)
 music.setTempo(120)
